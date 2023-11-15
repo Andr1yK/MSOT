52a8b7bf98e773898923265e1e52260e667cbcc3
Andriy Kobetiak <andriykobetiak@gmail.com>
2023-11-15 21:29:34 +0200

    fix-tests-for-euclideanAlgorithm

--- lab2/euclideanAlgorithm.test.ts
+++ lab2/euclideanAlgorithm.test.ts
@@ -3,13 +3,20 @@ import * as faker from "faker";
 
 describe('Euclid\'s Algorithm', () => {
   test('Should find the greatest common divisor of two numbers', () => {
-    expect(euclideanAlgorithm(0, 0)).toBe(1);
-    expect(euclideanAlgorithm(0, 5)).toBe(1);
-    expect(euclideanAlgorithm(5, 0)).toBe(1);
     expect(euclideanAlgorithm(5, 5)).toBe(5);
     expect(euclideanAlgorithm(10, 5)).toBe(5);
     expect(euclideanAlgorithm(14, 28)).toBe(14);
     expect(euclideanAlgorithm(21, 14)).toBe(7);
+  });
+
+  test('Should find the greatest common divisor when one of the numbers is zero', () => {
+    // If k is a non-zero integer, then k divides zero. ... So gcd(k, 0) = gcd(0,k) = k. However, gcd(0, 0) isn't defined.
+    // All integers are common divisors of 0 and 0, so there is no greatest one.
+    // source: http://mfleck.cs.illinois.edu/building-blocks/version-1.0/number-theory.pdf
+
+    expect(euclideanAlgorithm(5, 0)).toBe(euclideanAlgorithm(0, 5));
+    expect(euclideanAlgorithm(5, 0)).toBe(5);
+    expect(euclideanAlgorithm(0, 5)).toBe(5);
   });
 
   // Generate random test cases
